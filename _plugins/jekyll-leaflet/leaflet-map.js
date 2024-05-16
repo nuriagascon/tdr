cca78df42db40f556813400bd11a916b607441a2
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-05-16 14:03:01 +0200

    feat: test with panes

--- _plugins/jekyll-leaflet/leaflet-map.js
+++ _plugins/jekyll-leaflet/leaflet-map.js
@@ -9,44 +9,44 @@
 
     // Override the map div id if specified, apply default CSS
     var defaultMapElId = "leaflet-map-%{id}";
-   var defaultMapElStyle = "height:300px; margin-top:15px; margin-bottom:15px";
+    var defaultMapElStyle = "height:300px; margin-top:15px; margin-bottom:15px";
     var mapEl = document.getElementById(defaultMapElId);
-    if('divId' in tagInputArg){
+    if ('divId' in tagInputArg) {
         mapEl.id = tagInputArg['divId'];
     }
     var defaultMapCssEl = document.createElement("style");
-    defaultMapCssEl.innerHTML = 
+    defaultMapCssEl.innerHTML =
         "#" + defaultMapElId + "{" + defaultMapElStyle + "}";
     document.head.appendChild(defaultMapCssEl);
 
     var newWindowImgSrcBase64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAB3RJTUUH4wUVFzAHq2j99AAAAV9JREFUSMdjYKAxYCRHk5ubG9+1a9fm/v//H6cadnb2G/fu3atlIceC379/c3z9+jUEn5q/f/8eYWBgYECxQE5OroqA2Y8ePXq0hJWV9Qc3N/caZB8wMjJKff361QpvEAkKCv4nYMGh9+/f26MLamhoyLx9+3b/nz9/VJCC6MiLFy9sWXCE3xlGRsZvWKQu4DOciYnpCzc396HPnz97weRxWRD/8OHDa4TiAt1wfn5+z79//zoxMDDALWAiN/lhM/zevXtH0NUx0dJwsiwgxXCccUCJ4dzc3DP+//+/gZ2d/duLFy9Is+Djx4+rCbn82rVrrxgYGF6RFUR8fHyZbGxsD7AZLiEhUSAiIrJSUlIyjOw4uHnz5gVHR0d1bC7/8+eP5d+/f8P+/PmjS1Ekr1ix4hcp6ploXVwPfQuwJlMmJiYBLS0tIVIMev36NRvRFnz48OHohw8fhkYQjQKCAAChiL6Pj/LM2QAAAABJRU5ErkJggg==";
 
-// Actual mapping section; Specify a function to be called later that 
-// assembles the correct JS components based on what the user specified in the 
+// Actual mapping section; Specify a function to be called later that
+// assembles the correct JS components based on what the user specified in the
 // tag input arg, the block section of features, etc. Actually creates the map
 // that is visible on the page
 
-    function _getCenter(){
-        var defaultCenter = [0,0];
-        if("center" in tagInputArg){
+    function _getCenter() {
+        var defaultCenter = [0, 0];
+        if ("center" in tagInputArg) {
             return tagInputArg.center;
         } else {
             return defaultCenter;
         }
     }
 
-    function _getZoom(){
+    function _getZoom() {
         var defaultZoom = 1;
-        if("zoom" in tagInputArg){
+        if ("zoom" in tagInputArg) {
             return tagInputArg.zoom
         } else {
             return defaultZoom;
         }
     }
 
-    function _getProviderBasemap(){
+    function _getProviderBasemap() {
         var defaultProviderBasemap = "OpenStreetMap.Mapnik";
-        if("providerBasemap" in tagInputArg){
+        if ("providerBasemap" in tagInputArg) {
             return tagInputArg.providerBasemap;
         } else {
             return defaultProviderBasemap;
@@ -54,39 +54,43 @@
     }
 
 
-    function _addMarkerToMap(leafletItem, map){
+    function _addMarkerToMap(leafletItem, map) {
         var m = leafletItem.value;
         var result = L.marker([m.latitude, m.longitude]).addTo(map);
         var potentialPopup = "";
-        if('popupContent' in m){
-                potentialPopup += m.popupContent;}
-        if('href' in m){
-            potentialPopup += '<a href="' + m.href + '">' + 
-                '<img src="' + newWindowImgSrcBase64 + '"></img></a>';}
-        if(potentialPopup){
-            result.bindPopup(potentialPopup);}
-        if(!('center' in tagInputArg)){
+        if ('popupContent' in m) {
+            potentialPopup += m.popupContent;
+        }
+        if ('href' in m) {
+            potentialPopup += '<a href="' + m.href + '">' +
+                '<img src="' + newWindowImgSrcBase64 + '"></img></a>';
+        }
+        if (potentialPopup) {
+            result.bindPopup(potentialPopup);
+        }
+        if (!('center' in tagInputArg)) {
             // If the user didn't specify a center, infer from marker
             map.panTo(new L.LatLng(m.latitude, m.longitude));
         }
     }
 
-    function _onEachFeature(feature, layer){
+    function _onEachFeature(feature, layer) {
         var potentialPopup = ""
-        if(feature.properties && feature.properties.popupContent){
+        if (feature.properties && feature.properties.popupContent) {
             potentialPopup += feature.properties.popupContent;
         }
-        if(feature.properties && feature.properties.href){
-            potentialPopup += 
-                '<a href="' + feature.properties.href + '">' + 
-                    '<img src="' + newWindowImgSrcBase64 + '"></img></a>';}
-        if(potentialPopup){
+        if (feature.properties && feature.properties.href) {
+            potentialPopup +=
+                '<a href="' + feature.properties.href + '">' +
+                '<img src="' + newWindowImgSrcBase64 + '"></img></a>';
+        }
+        if (potentialPopup) {
             layer.bindPopup(potentialPopup);
         }
     }
 
-    function _addGeoJSONToMap(leafletItem, map){
-        if(typeof leafletItem.value === "string"){
+    function _addGeoJSONToMap(leafletItem, map) {
+        if (typeof leafletItem.value === "string") {
             fetch(leafletItem.value).then((resp) => {
                 return resp.json();
             }).then((data) => {
@@ -100,31 +104,62 @@
         } else {
             _addGeoJSONObjToMap(leafletItem, map);
         }
-     }
-
-    function _addGeoJSONObjToMap(leafletItem, map){
-         var geojson = L.geoJSON(leafletItem.value, { 
-            onEachFeature: _onEachFeature 
-        });
-        layers = geojson.getLayers();
-        for(var i=0; i<layers.length; i++){
-            try{
-                var geom = layers[i].feature.geometry;
-                if(!('center' in tagInputArg)){
-                    // If the user didn't specify a center, infer from geojson
-                    //console.log("panning to " + geom.coordinates);
-                    map.panTo(new L.LatLng(geom.coordinates[1],
-                                           geom.coordinates[0]));
-                }
-            } catch(e){}
-        }
-       
-        geojson.addTo(map);
-       
     }
 
-    function _processLeafletItem(leafletItem, map){
-        switch(leafletItem.type){
+    function _createPane(map, name) {
+        switch (name){
+            case "bcil-existents":
+                map.createPane("pane_bcin_existents");
+                map.getPane("pane_bcin_existents").style.zIndex = 408;
+                map.getPane("pane_bcin_existents").style["mix-blend-mode"] = "normal";
+                break;
+            case "bcil-previstos-cbp":
+                map.createPane("pane_bcin_previstos");
+                map.getPane("pane_bcin_previstos").style.zIndex = 405;
+                map.getPane("pane_bcin_previstos").style["mix-blend-mode"] = "normal";
+                break;
+        }
+    }
+
+    function _style(name) {
+        return {
+            pane: name,
+            opacity: 1,
+            color: "rgba(35,35,35,1.0)",
+            dashArray: "",
+            lineCap: "butt",
+            lineJoin: "miter",
+            weight: 1.0,
+            fill: true,
+            fillOpacity: 1,
+            fillColor: "rgba(103,88,150,1.0)",
+            interactive: true,
+        }
+    }
+
+    function _addGeoJSONObjToMap(leafletItem, map) {
+        var geojson = L.geoJSON(leafletItem.value, {
+            onEachFeature: _onEachFeature,
+            style: _style("pane_bcin_existents")
+        });
+        _createPane(map, "bcil-existents");
+        layers = geojson.getLayers();
+        for (var i = 0; i < layers.length; i++) {
+            var geom = layers[i].feature.geometry;
+            if (!('center' in tagInputArg)) {
+                // If the user didn't specify a center, infer from geojson
+                //console.log("panning to " + geom.coordinates);
+                map.panTo(new L.LatLng(geom.coordinates[1],
+                    geom.coordinates[0]));
+            }
+        }
+
+        geojson.addTo(map);
+
+    }
+
+    function _processLeafletItem(leafletItem, map) {
+        switch (leafletItem.type) {
             case "LeafletMarker":
                 _addMarkerToMap(leafletItem, map);
                 break;
@@ -132,7 +167,7 @@
                 _addGeoJSONToMap(leafletItem, map);
                 break;
             case undefined:
-                break; 
+                break;
             default:
                 console.log("Couldn't add item " + leafletItem.id);
                 break;
@@ -140,20 +175,20 @@
     }
 
 //The actual section that is called that creates a map
-    function createMap(){
+    function createMap() {
         console.log("Creating map %{id} with these args:");
         console.log(tagInputArg);
 
         //Initialize Map with the correct input arguments
-        var map = L.map(mapEl.id, 
+        var map = L.map(mapEl.id,
             {worldCopyJump: true}).setView(_getCenter(), _getZoom());
         L.tileLayer.provider(_getProviderBasemap()).addTo(map);
-        if("esriBasemap" in tagInputArg){
+        if ("esriBasemap" in tagInputArg) {
             L.esri.basemapLayer(tagInputArg.esriBasemap).addTo(map);
         }
 
         //process each Leaflet Item passed in between the block tag middle
-        for(var i=0; i<blockLeafletItems.length; i++){
+        for (var i = 0; i < blockLeafletItems.length; i++) {
             var leafletItem = blockLeafletItems[i];
             console.log("Adding leaflet item " + leafletItem.id + ":");
             console.log(leafletItem);
@@ -161,19 +196,19 @@
         }
     }
 
-// Load the correct JS libraries/CSS by adding to head: 
+// Load the correct JS libraries/CSS by adding to head:
 // When ready, call createMap() to create the map on the page
 
-    function _createMap(){
+    function _createMap() {
         // helper function to draw the map only when everything is loaded,
         // safeguards against multiple calls to window.onload
         var prevOnLoad;
-        if(window.onload){
+        if (window.onload) {
             prevOnLoad = window.onload;
         }
         window.onload = () => {
             createMap();
-            if(prevOnLoad){
+            if (prevOnLoad) {
                 prevOnLoad();
             }
         }
@@ -189,14 +224,14 @@
     leafletCssEl.id = leafletCssId;
     leafletCssEl.rel = "stylesheet";
     leafletCssEl.href = leafletCdn + "leaflet.css";
-    if(!document.getElementById(leafletCssEl.id)){
+    if (!document.getElementById(leafletCssEl.id)) {
         document.head.appendChild(leafletCssEl);
     }
 
     function addToHeadIfNotLoaded(el) {
         //Add the el to the head if it doesn't exist already. If it does,
         //everything we need is already loaded, so draw the map
-        if(!document.getElementById(el.id)){
+        if (!document.getElementById(el.id)) {
             document.head.appendChild(el);
         } else {
             _createMap();
@@ -215,14 +250,14 @@
             provEl = document.createElement("script");
             provEl.id = leafletProvidersJsId;
             provEl.innerHTML = `%{leaflet_providers_js_content}`;
-            if(!document.getElementById(provEl.id)){
-                 document.head.appendChild(provEl);        
+            if (!document.getElementById(provEl.id)) {
+                document.head.appendChild(provEl);
             }
             _createMap();
-            }
+        }
         esriEl.src = esriLeafletCdn + "esri-leaflet.js";
         addToHeadIfNotLoaded(esriEl);
-        };
+    };
     leafletJsEl.src = leafletCdn + "leaflet.js";
     addToHeadIfNotLoaded(leafletJsEl);
 })();
