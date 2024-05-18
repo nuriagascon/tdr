3b3c3998f280aea332ae610a477deff92a8ce52a
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-05-18 17:47:21 +0200

    feat: search bar for mobile

---
+++ js/search/search-buttons.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const openPanelButton = document.getElementById("search-button");
+    const searchCancelButton = document.getElementById("search-cancel");
+
+    const searchInput = document.getElementById("search-input");
+
+    const header = document.querySelector(".header");
+
+    function clearSearchInput(){
+        searchInput.value = "";
+
+        let event = new Event('input', {
+            bubbles: true,
+            cancelable: true
+        });
+
+        searchInput.dispatchEvent(event);
+    }
+
+    openPanelButton.addEventListener("click", function() {
+        header.classList.add("mobile-search");
+        clearSearchInput();
+    });
+
+    searchCancelButton.addEventListener("click", function() {
+        header.classList.remove("mobile-search");
+        clearSearchInput();
+    });
+
+    addEventListener("resize", (event) => {
+        let width = window.innerWidth;
+
+        if (width > 1160) {
+            header.classList.remove("mobile-search");
+        }
+    });
+});
